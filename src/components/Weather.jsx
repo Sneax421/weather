1b0aa9053493bcc6fb48253a64d1c72c55a65a4e
Sneax421 <curilov.german@web.de>
2025-03-24 11:42:19 +0100

    Refactor Weather with &&

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+
 
 const Weather = ({weather, message}) => {
-    if (message) {
-        return (
-            <div className={'infoWeath'}>{message}</div>
-        )
-    } else {
-        return (
-            <div className={'infoWeath'}>
-                <p>Location: {weather.country}, {weather.city}</p>
-                <p>Temp: {weather.temp}</p>
-                <p>Pressure: {weather.pressure}</p>
-                <p>Sunset: {(new Date(weather.sunset * 1000)).toLocaleTimeString()}</p>
-            </div>
-        );
-    }
-};
+    return (
+        <div className={'infoWeath'}>
+            {!message &&
+                <>
+                    <p>Location: {weather.country}, {weather.city}</p>
+                    <p>Temp: {weather.temp}</p>
+                    <p>Pressure: {weather.pressure}</p>
+                    <p>Sunset: {(new Date(weather.sunset * 1000)).toLocaleTimeString()}</p>
+                </>
+            }
+            {message}
+        </div>
+    )
+}
 
 export default Weather;
+
+
+
+
